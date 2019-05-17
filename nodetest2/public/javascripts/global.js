9b759bb6329501bdf233510e7362df6a44961af5
Logan Willett <lwillett@pgsioffice.com>
2019-05-17 14:17:38 -0400

    Added functionality to add new users to the database.

--- nodetest2/public/javascripts/global.js
+++ nodetest2/public/javascripts/global.js
@@ -9,6 +9,9 @@ $(document).ready(function() {
 
   //Username Link click
   $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
+  //Add user button click
+  $('#btnAddUser').on('click', addUser);
 });
 
 // Functions =============================================================
@@ -36,7 +39,7 @@ function populateTable() {
 
     // Inject the whole content string into our existing HTML table
     $('#userList table tbody').html(tableContent);
-    $('#userList').css('color', 'red');
+    $('#userList').css('color', 'black');
     $('#userList').css('font-size', 'larger')
   });
 };
@@ -58,4 +61,52 @@ function showUserInfo(event) {
   $('#userInfoAge').text(thisUserObject.age);
   $('#userInfoGender').text(thisUserObject.gender);
   $('#userInfoLocation').text(thisUserObject.location);
+};
+
+//Add user
+function.addUser(event) {
+  event.preventDefault();
+
+  //Basic validation - Increases errorCount if any fields are blank
+  var errorCount = 0;
+  $('#addUser input').each(function(index, val) {
+    if($(this).val() === '') { errorCount++ }
+  });
+
+  //Check and make sure errorCount is still at zero
+  if (errorCount === 0) {
+    //If there are no blank fields, compile all data into one object
+    var newUser = {
+      'username': $('#addUser fieldset input#inputUserName').val();
+      'email': $('#addUser fieldset input#inputUserEmail').val();
+      'fullname': $('#addUser fieldset input#inputUserFullname').val();
+      'age': $('#addUser fieldset input#inputUserAge').val();
+      'location': $('#addUser fieldset input#inputUserLocation).val();
+      'gender': $('#addUser fieldset input#inputUserGender').val();
+    }
+
+    //Use AJAX to post the object to the adduser service
+    $.ajax({
+      type: 'POST',
+      data: newUser,
+      url: '/users/adduser',
+      dataType: 'JSON'
+    }).done(function( response ) {
+      //Check for blank response (successful)
+      if (response.msg === '') {
+        //Clear form inputs
+        $('#addUser fieldset input').val('');
+        //Update the table
+        populateTable();
+      }
+      else {
+        alert('Error: ' + response.msg);
+      }
+    });
+  }
+  else {
+    //If errorCount is more than 0, error out
+    alert('Please fill in all fields');
+    return false;
+  }
 };
