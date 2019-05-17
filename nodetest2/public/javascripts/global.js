7c915ef1e0c169e7e80375e92fee13e334094a3b
Logan Willett <lwillett@pgsioffice.com>
2019-05-17 13:01:35 -0400

    Added functionality to extract data pertaining to specific users and show that.

--- nodetest2/public/javascripts/global.js
+++ nodetest2/public/javascripts/global.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
   // Populate the user table on initial page load
   populateTable();
 
+  //Username Link click
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 // Functions =============================================================
@@ -20,10 +22,13 @@ function populateTable() {
   // jQuery AJAX call for JSON
   $.getJSON( '/users/userlist', function( data ) {
 
+    //Puts the data array into a userlist variable in the global object
+    userListData = data;
+
     // For each item in our JSON, add a table row and cells to the content string
     $.each(data, function(){
       tableContent += '<tr>';
-      tableContent += '<td><button><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></button></td>';
+      tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
       tableContent += '<td>' + this.email + '</td>';
       tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
       tableContent += '</tr>';
@@ -32,5 +37,25 @@ function populateTable() {
     // Inject the whole content string into our existing HTML table
     $('#userList table tbody').html(tableContent);
     $('#userList').css('color', 'red');
+    $('#userList').css('font-size', 'larger')
   });
+};
+
+//Shows user info
+function showUserInfo(event) {
+  //Prevents a link from firing
+  event.preventDefault();
+  //Retrieves username from link rel attribute
+  var thisUserName = $(this).attr('rel');
+  //Gets an index of objects based on the id value
+  var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+  //Gets the user object
+  var thisUserObject = userListData[arrayPosition];
+
+  //Populates the info box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
 };
